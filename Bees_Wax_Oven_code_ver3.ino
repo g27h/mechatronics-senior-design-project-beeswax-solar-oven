c781510cca6ea89ed808ccf5e8a6b8dd6f81eb1b
g27h <gus27h@vt.edu>
2020-02-29 22:24:44 -0500

    Beeswax Oven code ver3
    -Completed driveMotor() function, making it ready for testing.
    -Edited sunTrackingModeFunct(), which is now ready for testing.
    -Code linking physical Arduino pins to the code is now in the setup() function.

--- Bees_Wax_Oven_code_ver3.ino
+++ Bees_Wax_Oven_code_ver3.ino
@@ -3,12 +3,20 @@
 // Methodology for the Code: 
 //  1. Receive both signals from the solar photocells and use an ADC on the Arduino to turn them into digital signals
 //  2. Use a simple differential variable to hold the difference between the two photocell values
-//  3. Have a P controller act upon the difference on the photocell values
-//    3a. 
-//  4. Have a P controller for the Temperature setpoint to change the Voltage setpoint for the other PI controller
+//  3. Have a P controller act upon the difference on the photocell values while in the "Sun Tracking Mode"
+//    3a. Calculates the PWM value based on the k_p_V constant and voltage error from the setpoint
+//  4. Have a P controller for the Temperature setpoint to change the Voltage setpoint for the other P controller
+//    4a. Calculates the new setpoint value for the other P controller based on the k_p_T constant, the Temperature error and the voltage ratio
+//    4b. The voltage ratio is used to determine the sign of the setpoint and a small fraction of the magnitude
+//  5. TODO: Finally the code features a "Return to Home" mode which can be activated via an interrupt from a button or strictly from the day ending
+//    5a. TODO: The method of having the solar oven return to home is somewhat unclear, and may deal with the voltages reaching a low value
+//    5b. TODO: May need an encoder and two buttons to help set the angular limits of the solar oven
 // Methodology for the Hardware:
 //  1. Have capacitors on the photocells to act as a low-pass filter
-
+//  2. Use a Qunqi L298N motor controller board (which has the capacity of up to 2 motors)
+//    2a. The motor controller board is capable of running up to 12V and has a regulator
+//      2a1. The motor runs up to 12V (though we will not be running strictly at that voltage ever)
+//      2a2. The 5V supply can be used to power the arduino itself
 
 
 void VariableSetup() {
@@ -27,22 +35,22 @@ void VariableSetup() {
   static volatile uint16_t V_cell_R; //The voltage from photocell 2 (right)
   static volatile uint16_t V_diff; //The difference in voltage of the photocells
   static volatile uint16_t V_setpoint; //The desired voltage difference setpoint. Initially this is zero which is unity between the two photocells
-  static volatile uint16_t V_setpoint_mod; //setpoint modifier
   
   //Numerical Variables - Temperature
   static volatile uint16_t T_bed; //The temperature in the bed
   static volatile uint16_t T_outside; //The outside temperature
-  static volatile uint16_t T_setpoint_high; //The desired max temperature for the wax to operate at
-  static volatile uint16_t T_setpoint_low; //The desired max temperature for the wax to operate at
   static volatile uint16_t W_bed; //The weight of the bed
 
   //Numerical Variables - Errors
   static volatile uint16_t V_error;
   static volatile uint16_t T_error;
 
-  //PWM / Motor Variables
+  //Motor Variables
   static volatile uint16_t PWM;
   static volatile bool driveDirection;
+  static volatile int16_t enA;
+  static volatile int16_t in1;
+  static volatile int16_t in2;
 }
 
 void setup() {
@@ -54,16 +62,28 @@ void setup() {
   ReturnToHome = false;
 
   V_setpoint = 0; //0V setpoint in the beginning
-  T_setpoint = 65;
+  T_setpoint = 65; //65 deg F setpoint
 
-  k_p_V = 50;
-  k_p_T = 10;
-  
+  k_p_V = 50; //arbitrary kp constant
+  k_p_T = 10; //arbitrary kp constant
+
+  // Motor variables
+  // The motor is controlled via a HIGH to IN1 and a LOW to IN2 to turn in one dir
+  // or a LOW to IN1 and a HIGH to IN2. In the cases of sending the high signal
+  // we instead use PWM
+  enA = 10; //PWM/analog pin 10, connected to module pin 7 (remove the jumper first)
+  in1 = 9; //digital pin 9, connected to IN1
+  in2 = 8; //digital pin 8, connected to IN2
+
+  //Setting pins 10, 9 and 8 as outputs
+  pinMode(enA, OUTPUT);
+  pinMode(in1, OUTPUT);
+  pinMode(in2, OUTPUT);
 }
 
 void loop() {
-  //Read left voltage
-  //Read right voltage
+  //TODO: Read left voltage and store it using the ADC
+  //TODO: Read right voltage and store it using the ADC
 
   V_diff = V_cell_L - V_cell_R; //left - right sensor voltages, thus this will typically be positive
   
@@ -90,33 +110,40 @@ void loop() {
   }
   //May need an encoder for the return home variable and a push button to home the oven
   else if(returnHome){
-    //Runs the motor to the home position
+    //TODO: Runs the motor to the home position
   }
 }
 
-//Created to 
+//Created to be a standalone function for tracking the sun. Will be used in the 
+//temperature controlling mode as well.
 void sunTrackingModeFunct(){
   PWM = abs(k_p_V*V_error);
-  if(PWM > 99){
-    PWM = 100;
+  if(PWM > 255){ //the range on an arduino is 0~255
+    PWM = 255;
   }
   if(V_error < 0){ //more sunlight on the left cell
     //drive the motor to move the oven CW
-    driveMotor(true,PWM);
+    driveMotor(true, PWM);
   }
   else{ //more sunlight on the right cell
     //drive the motor to move the oven CCW
-    driveMotor(false,PWM);
-    
+    driveMotor(false, PWM);
   }
 }
 
 void driveMotor(bool driveDirection, uint16_t PWM){
   if(driveDirection = true){
-    //drive the motor CW
+    //drive the motor CW using a PWM pin, also a motor control direction pin
+    //TODO: guessing here on the drive direction for the in1 and in2..
+    analogWrite(enA, PWM);
+    digitalWrite(in1, HIGH);
+    digitalWrite(in2, LOW);
   }
   else{ //direction = false
-    //drive the motor CCW
+    //drive the motor CCW using a PWM pin, also a motor control direction pin
+    //TODO: guessing here on the drive direction for the in1 and in2..
+    analogWrite(enA, PWM);
+    digitalWrite(in1, LOW);
+    digitalWrite(in2, HIGH);
   }
 }
-
